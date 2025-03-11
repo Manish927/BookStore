fa7170162b01a40b9fa7f0a33b3824f370373f9c
Manish Srivastava <mannu927@yahoo.com>
2025-03-11 20:20:28 +0530

    Add files via upload
    Build functionality for BookStore
    main execution

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "src/main/bookstore/BookStoreApp.h"
+
+int main(int argc, char* argv[], char* environ[]) {
+    BookStoreApp app;
+    app.init();
+    //app.run();
+
+    return 0;
+}
