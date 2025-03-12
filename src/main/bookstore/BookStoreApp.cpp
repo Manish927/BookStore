08e5655feeb4855749d4b7cd7b841efcedf7cf56
Manish Srivastava <mannu927@yahoo.com>
2025-03-12 14:42:20 +0530

    Updated with all the CRUD operation

--- src/main/bookstore/BookStoreApp.cpp
+++ src/main/bookstore/BookStoreApp.cpp
@@ -1,10 +1,10 @@
 #include "BookStoreApp.h"
 
 void BookStoreApp::init() {
-    //book_store_service_.create_book();
+    book_store_service_.create_book();
+    book_store_service_.display_book();
     book_store_service_.read_book();
     book_store_service_.update_book();
-    // book_store_service_.delete_book();
     book_store_service_.display_book();
-
+    book_store_service_.delete_book();
 }
