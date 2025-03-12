8e44e954bdbe39e74045621e5e084fa7e625a71c
Manish Srivastava <mannu927@yahoo.com>
2025-03-12 14:50:05 +0530

    Connection details changes for child class

--- src/main/bookstore/repository/BookStoreRepo.h
+++ src/main/bookstore/repository/BookStoreRepo.h
@@ -5,18 +5,19 @@
 #include "PersistenceBaseRepo.h"
 #include "../entity/book.h"
 
-class BookStoreRepo : public PersistenceBaseRepo{
+class BookStoreRepo final : public PersistenceBaseRepo{
 
     public:
     explicit BookStoreRepo(const std::string& path);
+    explicit BookStoreRepo(const sw::redis::ConnectionOptions& opts, const sw::redis::ConnectionPoolOptions& pool_opts);
     ~BookStoreRepo() override ;
-    std::string fetchBook(std::string& book_id) override;
+    std::vector<std::string> fetchBook(std::string& book_id) override;
     std::string storeBook(Book& obj) ;
-    bool updateBook(std::string& book_id, std::unordered_map<std::string,std::string>& fields);
-    bool deleteBook(std::string& book_id);
+    void updateBook(std::string& book_id, std::unordered_map<std::string,std::string>& fields);
+    void deleteBook(std::string& book_id);
     void displayBooks() override;
+
+    void store_book(const std::string& , const std::unordered_map<std::string, std::string >&);
 };
-
-
 
 #endif //BOOKSTOREREPO_H
